01f01dcb179d6ca20bff44253aa81fa832e5295a
NazrinGasimova <165589947+NazrinGasimova@users.noreply.github.com>
2025-04-26 17:28:23 +0400

    Update Passenger.java

--- src/main/java/airlineapp/model/Passenger.java
+++ src/main/java/airlineapp/model/Passenger.java
@@ -1,16 +1,30 @@
 package airlineapp.model;
 
-public class Passenger {
-    private String name;
-    private int[] booking_ids = new int[900];
-    private int amount_of_bookings = 0;
+import java.util.ArrayList;
+import java.util.List;
 
-    public Passenger(String name){
-        this.name=name;
+public class Passenger {
+    private final String name;
+    private final List<Integer> bookingIds;
+
+    public Passenger(String name) {
+        this.name = name;
+        this.bookingIds = new ArrayList<>();
     }
 
     public String getName() {
         return name;
     }
 
+    public void addBooking(int bookingId) {
+        bookingIds.add(bookingId);
+    }
+
+    public List<Integer> getBookingIds() {
+        return new ArrayList<>(bookingIds);
+    }
+
+    public int getBookingCount() {
+        return bookingIds.size();
+    }
 }
